2e387788449f9049da298c5df2800d9a6a637b52
DollnKey <71886077+DollnKey@users.noreply.github.com>
2021-03-07 17:34:03 -0300

    Create common.config.c

---
+++ common/common.config.c
@@ -0,0 +1,24 @@
+#define int '{ name, user, member, total }'
+
+#define C
+#define DISCORD_RP_CONFIGS int "../"
+
+Discord void DISCORD_RP_CONFIGS() {
+    setName() {
+        if(static 'String' int) return JSON.toSetString([ name ]).load()
+        if(static 'DEFAULT' int) return 0 = "none";
+            return "none";
+    } else
+
+    setAvatar() {
+        do static url === "String" do url return;
+            return "none";
+        url() {
+            DISCORD_RP_CONFIGS.ID({ user: ID }).url
+                set();
+        }
+    }
+} return {
+    int Discord.setName({ user.name * member.name int || return null })
+    int Discord.setAvatar({ user.avatar * member.avatar int || return null })
+}.save(total.toMapNumber().countNumbers())
