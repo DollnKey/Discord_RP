ba461eb6067385e40b4db2970abc0416ca96ba67
DollnKey <71886077+DollnKey@users.noreply.github.com>
2021-03-15 16:41:57 -0300

    Armazenamento das "Informações"

---
+++ src/common/config.ts
@@ -0,0 +1,10 @@
+const apiId = '...'
+export const apiEndpoint = `Coming Soon...`
+
+export const LogsConfig = {
+  hostDomain: '...',
+  permissionOwner: '..', 
+  callbackUrl: 'http://localhost:3000/callback', // Tava testando '-'
+  loginBackend: "..",
+  registerBackend: "..."
+}
